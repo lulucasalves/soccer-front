8d2f15600ffc632a788614c64c1c21d04515aeb7
Lucas <lucas.alves.supus@outlook.com>
2023-05-15 10:29:53 -0300

    refactor: add colors

--- src/components/Matches/index.tsx
+++ src/components/Matches/index.tsx
@@ -6,15 +6,13 @@ import { IRootState } from "~/redux";
 
 export function Matches() {
   const { times, proximos_jogos } = data;
-  const { numberGames, sort } = useSelector((auth: IRootState) => auth.filters);
+  const { numberGames } = useSelector((auth: IRootState) => auth.filters);
 
   return (
     <div className="teams">
       {proximos_jogos.length &&
         proximos_jogos.map((teams, key) => {
           const teamsfiltered = times.filter((val) => {
-            console.log(getMatchs(teams.casa));
-
             return (
               val.clube.replaceAll(" ", "") ===
               getMatchs(teams.casa).replaceAll(" ", "")
@@ -22,8 +20,6 @@ export function Matches() {
           });
 
           const teamsfiltered2 = times.filter((val) => {
-            console.log(getMatchs(teams.fora));
-
             return (
               val.clube.replaceAll(" ", "") ===
               getMatchs(teams.fora).replaceAll(" ", "")
@@ -35,36 +31,41 @@ export function Matches() {
             mediaGolsFeitos,
             mediaGolsSofridos,
             mediaChutes,
-            mediaCartoes,
             sgTotalC,
-            sgTotalF,
             mediaGolsFeitosC,
-            mediaGolsFeitosF,
             mediaGolsSofridosC,
-            mediaGolsSofridosF,
             mediaChutesC,
-            mediaChutesF,
-            clube,
-            penaltisNumeros,
-          } = returnAllStats(teamsfiltered[0], numberGames);
+          } = returnAllStats(teamsfiltered[0], 5);
 
           const {
             sgTotal: sgTotal2,
             mediaGolsFeitos: mediaGolsFeitos2,
             mediaGolsSofridos: mediaGolsSofridos2,
             mediaChutes: mediaChutes2,
-            mediaCartoes: mediaCartoes2,
-            sgTotalC: sgTotalC2,
             sgTotalF: sgTotalF2,
-            mediaGolsFeitosC: mediaGolsFeitosC2,
             mediaGolsFeitosF: mediaGolsFeitosF2,
-            mediaGolsSofridosC: mediaGolsSofridosC2,
             mediaGolsSofridosF: mediaGolsSofridosF2,
-            mediaChutesC: mediaChutesC2,
             mediaChutesF: mediaChutesF2,
-            clube: clube2,
-            penaltisNumeros: penaltisNumeros2,
-          } = returnAllStats(teamsfiltered2[0], numberGames);
+          } = returnAllStats(teamsfiltered2[0], 5);
+
+          const generalResultC =
+            (mediaGolsFeitos + mediaGolsSofridos2) / 2 - 0.1;
+
+          const generalResultF =
+            (mediaGolsSofridos + mediaGolsFeitos2) / 2 - 0.1;
+
+          const resultC =
+            ((mediaGolsFeitosC + mediaGolsSofridosF2) / 2 + generalResultC) /
+              2 -
+            0.1;
+          const resultF =
+            ((mediaGolsSofridosC + mediaGolsFeitosF2) / 2 + generalResultF) /
+              2 -
+            0.1;
+
+          const sgTeam = sgTotal >= sgTotal2 ? teams.casa : teams.fora;
+          const sgDiff =
+            sgTotal >= sgTotal2 ? sgTotal - sgTotal2 : sgTotal2 - sgTotal;
 
           return (
             <div key={key} className="cards">
@@ -97,19 +98,59 @@ export function Matches() {
                     >
                       Média de gols sofridos: {mediaGolsSofridos.toFixed(1)}
                     </p>
-                    <p>SG Total em casa: {sgTotalC.toFixed(1)}</p>
+                    <p
+                      className={
+                        mediaChutes >= mediaChutes2 ? "positive" : "negative"
+                      }
+                    >
+                      Média chutes ao gol: {mediaChutes.toFixed(1)}
+                    </p>
+                    <p
+                      className={
+                        sgTotalC >= sgTotalF2 ? "positive" : "negative"
+                      }
+                    >
+                      SG Total em casa: {sgTotalC.toFixed(1)}
+                    </p>
                   </div>
                   <div className="double">
-                    <p>
+                    <p
+                      className={
+                        mediaGolsFeitosC >= mediaGolsFeitosF2
+                          ? "positive"
+                          : "negative"
+                      }
+                    >
                       Média de gols feitos em casa:{" "}
                       {mediaGolsFeitosC.toFixed(1)}
                     </p>
-                    <p>
+                    <p
+                      className={
+                        mediaGolsSofridosC <= mediaGolsSofridosF2
+                          ? "positive"
+                          : "negative"
+                      }
+                    >
                       Média de gols sofridos em casa:{" "}
                       {mediaGolsSofridosC.toFixed(1)}
                     </p>
+                    <p
+                      className={
+                        mediaChutesC >= mediaChutesF2 ? "positive" : "negative"
+                      }
+                    >
+                      Média chutes ao gol: {mediaChutesC.toFixed(1)}
+                    </p>
                   </div>
                 </div>
+              </div>
+              <div className="card result">
+                <h2>
+                  {Math.round(resultC)}x{Math.round(resultF)}
+                </h2>
+                <p className={sgDiff >= 10 ? "positive" : "negative"}>
+                  {sgTeam} {sgDiff}+ sg
+                </p>
               </div>
               <div className="card">
                 <p className="nome">{getMatchs(teams.fora)}</p>
@@ -140,16 +181,48 @@ export function Matches() {
                     >
                       Média de gols sofridos: {mediaGolsSofridos2.toFixed(1)}
                     </p>
-                    <p>SG Total fora de casa: {sgTotalF2}</p>
+                    <p
+                      className={
+                        mediaChutes <= mediaChutes2 ? "positive" : "negative"
+                      }
+                    >
+                      Média chutes ao gol: {mediaChutes2.toFixed(1)}
+                    </p>
+                    <p
+                      className={
+                        sgTotalC <= sgTotalF2 ? "positive" : "negative"
+                      }
+                    >
+                      SG Total fora de casa: {sgTotalF2}
+                    </p>
                   </div>
                   <div className="double">
-                    <p>
+                    <p
+                      className={
+                        mediaGolsFeitosC <= mediaGolsFeitosF2
+                          ? "positive"
+                          : "negative"
+                      }
+                    >
                       Média de gols feitos fora de casa:{" "}
                       {mediaGolsFeitosF2.toFixed(1)}
                     </p>
-                    <p>
+                    <p
+                      className={
+                        mediaGolsSofridosC >= mediaGolsSofridosF2
+                          ? "positive"
+                          : "negative"
+                      }
+                    >
                       Média de gols sofridos fora de casa:{" "}
                       {mediaGolsSofridosF2.toFixed(1)}
+                    </p>
+                    <p
+                      className={
+                        mediaChutesC <= mediaChutesF2 ? "positive" : "negative"
+                      }
+                    >
+                      Média chutes ao gol: {mediaChutesF2.toFixed(1)}
                     </p>
                   </div>
                 </div>
